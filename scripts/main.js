f6a02d76d3172bbdb13a01412712a65f8e0b88d9
Sandeep K K <sandeepdotcode@gmail.com>
2021-11-13 08:00:54 +0530

    Add operate() fn

--- scripts/main.js
+++ scripts/main.js
@@ -13,3 +13,23 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(operator, num1, num2) {
+    switch(operator) {
+        case '+':
+            result = add(num1, num2);
+            break;
+        case '-':
+            result = subtract(num1, num2);
+            break;
+        case '*':
+            result = multiply(num1, num2);
+            break;
+        case '/':
+            result = divide(num1, num2);
+            break;
+        default:
+            result = "ERROR";
+    }
+    return result;
+}
